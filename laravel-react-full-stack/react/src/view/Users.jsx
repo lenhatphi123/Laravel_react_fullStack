925e408e5f3f09142e115352db08bc380d524b6f
lenhatphi123 <lenhatphi123@gmail.com>
2023-09-17 22:57:30 +0700

    Created API CRUD

--- laravel-react-full-stack/react/src/view/Users.jsx
+++ laravel-react-full-stack/react/src/view/Users.jsx
@@ -1,6 +1,27 @@
-import React from "react";
-
+import React, { useEffect, useState } from "react";
+import axiosClient from "../axios-client";
 const Users = () => {
+    const [user, setUsers] = useState();
+    const [loading, setLoading] = useState(false);
+
+    useEffect(() => {
+        getUsers();
+    }, []);
+
+    const getUsers = () => {
+        setLoading(true);
+        axiosClient
+            .get("/users")
+            .then(({ data }) => {
+                setLoading(false);
+                console.log(data);
+            })
+            .catch((error) => {
+                setLoading(false);
+                console.log(error);
+            });
+    };
+
     return <div>Users</div>;
 };
 
